91f3ffb577bd8997d82e6a68442b84273c5a9de9
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-13 12:13:45 +0700

    Editing flying-circle

--- fly-circle.py
+++ fly-circle.py
@@ -1,4 +1,9 @@
+import pygame
+
 class Circle:
+    SCREEN_WIDTH = 100 
+    SCREEN_HIGHT = 100 
+
     def __init__(self,
                  radius: int | float,
                  x: int | float, y: int | float) -> None:
@@ -7,13 +12,46 @@ class Circle:
         self.y = y
 
     def draw(self) -> None:
+        pygame.draw.circle(surface=screen, center=(self.x, self.y), color=(255, 255, 255), radius=5) 
         print(f"{self.x}, {self.y}")
 
-    def move(self) -> None:
-        self.x += 1
-        self.y += 1
+    def move(self, forward: bool) -> bool:
+        if self.x < self.SCREEN_WIDTH and self.y < self.SCREEN_HIGHT and forward:
+            self.x += 1
+            self.y += 1
+            return True
 
+        elif self.x <= 0 or self.y <= 0:
+            return True
+
+        elif self.x >= self.SCREEN_WIDTH or self.y >= self.SCREEN_HIGHT or not forward:
+            self.x -= 1
+            self.y -= 1
+            return False
+
+# setup pygame
+pygame.init()
+pygame.display.set_caption("Flying-circle")
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((100, 100))
 
 if __name__ == "__main__":
     circle = Circle(10, 100, 100)
-    circle.draw()
+    forward = True
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+        # reset screen
+        screen.fill((0, 0, 0))
+
+        # main game
+        forward = circle.move(forward)
+        circle.draw()
+        
+        # game update frame by frame
+        pygame.display.update()
+        clock.tick(60)
