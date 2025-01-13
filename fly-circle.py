46753814aa35a1f8d5b88784b44103d753896556
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-13 14:23:04 +0700

    Finish Homework

--- fly-circle.py
+++ fly-circle.py
@@ -1,8 +1,8 @@
 import pygame
 
 class Circle:
-    SCREEN_WIDTH = 100 
-    SCREEN_HIGHT = 100 
+    SCREEN_WIDTH = 800
+    SCREEN_HIGHT = 600
 
     def __init__(self,
                  radius: int | float,
@@ -12,46 +12,46 @@ class Circle:
         self.y = y
 
     def draw(self) -> None:
-        pygame.draw.circle(surface=screen, center=(self.x, self.y), color=(255, 255, 255), radius=5) 
+        pygame.draw.circle(surface=screen, center=(
+            self.x, self.y), color=(255, 255, 255), radius=self.radius)
         print(f"{self.x}, {self.y}")
 
-    def move(self, forward: bool) -> bool:
-        if self.x < self.SCREEN_WIDTH and self.y < self.SCREEN_HIGHT and forward:
-            self.x += 1
-            self.y += 1
-            return True
+    def move(self, vx: int, vy: int) -> bool:
+        self.x += vx
+        self.y += vy
 
-        elif self.x <= 0 or self.y <= 0:
-            return True
-
-        elif self.x >= self.SCREEN_WIDTH or self.y >= self.SCREEN_HIGHT or not forward:
-            self.x -= 1
-            self.y -= 1
-            return False
 
 # setup pygame
 pygame.init()
 pygame.display.set_caption("Flying-circle")
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((100, 100))
+screen = pygame.display.set_mode((800, 600))
 
 if __name__ == "__main__":
     circle = Circle(10, 100, 100)
-    forward = True
+
+    # velocity
+    vx, vy = 5, 5
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+
         # reset screen
         screen.fill((0, 0, 0))
 
         # main game
-        forward = circle.move(forward)
+        if circle.x > circle.SCREEN_WIDTH or circle.x < 0:
+            vx = -vx
+
+        if circle.y > circle.SCREEN_HIGHT or circle.y < 0:
+            vy = -vy
+
+        circle.move(vx, vy)
         circle.draw()
-        
+
         # game update frame by frame
         pygame.display.update()
         clock.tick(60)
